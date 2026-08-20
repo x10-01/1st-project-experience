b7ad6f8e55323fefc220f7017d67af45aa9f6777
X10 <xuyiling0608@163.com>
2026-08-21 02:05:26 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+from machine import LED
+
+led = LED("LED_BLUE")
+
+while True:
+    led.on()
+    time.sleep_ms(150)
+    led.off()
+    time.sleep_ms(100)
+    led.on()
+    time.sleep_ms(150)
+    led.off()
+    time.sleep_ms(600)
